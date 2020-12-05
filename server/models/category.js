cc8967703dc4ddd7055c40d18d8d4ac4719ecbc5
warry0 <fathalla.amine@free.fr>
2020-12-05 19:50:28 +0100

    correction d'erreur

--- server/models/category.js
+++ server/models/category.js
@@ -5,4 +5,4 @@ const CategorySchema = new Schema({
   type: { type: String, unique: true, required: true },
 });
 
-module.exports = mongoose.model("Category", CategorySchema);
+module.exports = mongoose.model("category", CategorySchema);
